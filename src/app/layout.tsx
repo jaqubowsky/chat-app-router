743e35243eaa6c06d3decdac2ca748da7b9db9b1
Jakub <jaqubowskyy@gmail.com>
2023-10-21 16:24:29 +0200

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "@/styles/globals.css";
+
+import { Inter } from "next/font/google";
+import { headers } from "next/headers";
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Chat App",
+  description: "Chat App",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${inter.variable}`}>
+        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
